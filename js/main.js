faf9f23cf114f7784e24398fb58c7bdd15ca1cdd
Arze1 <Arze1@users.noreply.github.com>
2017-06-30 22:25:27 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ $("#typetype").typed({
 	loop: true,
 	loopCount: Infinity
 });
+var dt = new Date();
+
+fetch(`https://api.npmjs.org/downloads/point/2017-06-29:${dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate()}/verifyr`).then(function(response) { 
+	return response.json();
+}).then(function(j) {
+	$(".downloads").html(j.downloads || "(ERROR)"); 
+});
